ca404cc0dd3b7bbc603f815643ee31f327a5603d
Ashwin P Nair <ashwinprakashnair@gmail.com>
2026-08-06 15:04:41 +0530

    Add smallestNumber function to find valid x

---
+++ SmNoDivPrdt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int digitProduct(int x) {
+        int prod = 1;
+        while (x > 0) {
+            prod *= (x % 10);
+            x /= 10;
+        }
+        return prod;
+    }
+
+    int smallestNumber(int n, int t) {
+        int x = n;
+        while (true) {
+            if (digitProduct(x) % t == 0)
+                return x;
+            x++;
+        }
+    }
+};
+
+int main() {
+    int n, t;
+
+    cout << "Enter n and t: ";
+    cin >> n >> t;
+
+    Solution obj;
+    cout << "Smallest number: " << obj.smallestNumber(n, t) << endl;
+
+    return 0;
+}
