0d5fafc628420e28ae58cd94184343684efdb56a
Ashwin P Nair <ashwinprakashnair@gmail.com>
2026-09-02 21:50:52 +0530

    Update SingleNumber_136.cpp

--- SingleNumber_136.cpp
+++ SingleNumber_136.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -8,3 +12,14 @@ public:
         return result;
     }
 };
+
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+
+    Solution sol;
+    int answer = sol.singleNumber(nums);
+
+    cout << "The single number is: " << answer << endl;
+
+    return 0;
+}
